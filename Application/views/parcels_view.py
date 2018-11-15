333ebfa82ef33de33a2525972c965b0936cd27b7
p@rick <frzpete@gmail.com>
2018-11-16 01:43:56 +0300

    Implemented user/admin login/signup
    Endpoints to implement user/admin login and signup have
    been added to improve authorisation in our application

--- Application/views/parcels_view.py
+++ Application/views/parcels_view.py
@@ -9,6 +9,7 @@ parcels =[
     {
         'parcel_id':1,
         'parcel_description':'Pumped Up Kicks & pinnata',
+        'user_id':1,
         'client': 'BUcky',
         'recipient':'Mart',
         'pickup_location':'Quality Shopping Village',
@@ -18,23 +19,174 @@ parcels =[
         'status':'Pending'
     }
 ]
-admin=[]
-users=[]
+admin_accounts=[]
+user_accounts=[]
 current_user =[]#store current user id
 current_admin =[]#store current admin id
 
+def logout_active_users():
+    current_admin.pop()
+    current_user.pop()
 
 @blue_print.route('/api/v1/admin/signup',methods = ['POST'])
 def admin_signup():
+    #logout_active_users()
     data = request.get_json()
-
     
+    admin_id=len(admin_accounts)+1
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    if type(username)!=str or type(email) !=str or type(password)!=str:
+        return jsonify({
+            'message':'sorry! All fields must be strings'
+        }),400
+
+    if not username or username.isspace():
+        return jsonify({
+            'message':'sorry! your username is required and can not be an empty string'
+        }), 400
+
+    if not email or email.isspace():
+        return jsonify({
+            'message':'sorry! email is required and can not be an empty string'
+        }), 400
+
+    if not password or password.isspace():
+        return jsonify({
+            'message':'sorry! your password is required and can not be an empty string'
+        }), 400
+
+    admin =dict(
+        admin_id =admin_id,
+        username = username,
+        email = email,
+        password = password
+    )
+    
+    admin_accounts.append(admin)
+
+    return jsonify({
+        'message': 'hello! '+admin['username']+' You Account has been created and automatically logged in',
+    }),200
+
+@blue_print.route('/api/v1/admin/login', methods = ['POST'])
+def admin_login():
+    if len(admin_accounts)==0:
+        return jsonify({
+            'message':'No accounts available yet'
+        }),400
+    data = request.get_json()
+    
+    email = data.get('email')
+    password = data.get('password')
+
+    i=0#keeps track of admin index
+    for existing_account in admin_accounts:
+        if email==existing_account['email']:#if email exists
+            if password==existing_account['password']:#if password matches
+                current_admin.append(i)#store id of loggedin admin
+                return jsonify({
+                    'message':'Hello! '+existing_account['username']+' You are logged in'
+                }),200
+            return jsonify({
+                'message':'Invalid email or password'
+            }),400
+        i+=1 
+    
+@blue_print.route('/api/v1/signup', methods = ['POST'])
+def signup():
+    
+    data = request.get_json()
+    user_id = len(user_accounts)+1
+    username = data.get('username')
+    email = data.get('email')
+    password =data.get('password')
+
+    if type(username)!=str or type(email)!=str or type(password)!=str:
+        return jsonify({
+            'message':'All information should be a sequence of characters(String type)'
+        }),400
+    
+    if not username or not email or not password:
+        return jsonify({
+            'message':'Information missing, make sure username,email and password are passed'
+        }),400
+    
+    if username.isspace() or email.isspace() or password.isspace():
+        return jsonify({
+            'message':'make sure all fields have information. no field can be an empty space'
+        }),400
+    
+    for existing_account in user_accounts:
+        if existing_account['email']==email:
+            return jsonify({
+                'message':'Email already exists'
+            }),400
+
+    user = dict(
+        user_id =user_id,
+        username=username,
+        email=email,
+        password=password
+    )
+    current_user.append(user_id)
+    user_accounts.append(user)
+    return jsonify({
+        'message':'user has been added and logged in'
+    })
+    
+@blue_print.route('/api/v1/login',methods=['POST'])
+def login():
+    logout_active_users()
+    data = request.get_json()
+    
+    
+    email = data.get('email')
+    password =data.get('password')
+
+    if type(email)!=str or type(password)!=str:
+        return jsonify({
+            'message':'All information should be a sequence of characters(String type)'
+        }),400
+    
+    if not email or not password:
+        return jsonify({
+            'message':'Information missing, make sure username,email and password are passed'
+        }),400
+    
+    if email.isspace() or password.isspace():
+        return jsonify({
+            'message':'make sure all fields have information. no field can be an empty space'
+        }),400
+    
+    i=0
+    for existing_account in user_accounts:
+        if existing_account['email']==email:
+            if existing_account['password']==password:
+                current_user.append(i)
+                return jsonify({
+                    'message':existing_account['user']+'you are logged in'
+                }),200
+            return jsonify({
+                'message':'invalid email or password'
+            }),400
+        i+=1
+
+    return jsonify({
+        'message':'Invalid email address or password'
+    }),400
+   
 
 @blue_print.route('/api/v1/parcels')
 def getParcels():
-   return jsonify({
-       'parcels':parcels
-   })
+    if len(parcels)>0:
+        return jsonify({
+        'parcels':parcels
+        }),200
+    return jsonify({
+        'message':'No parcels added yet'
+    }),400
 
 @blue_print.route('/api/v1/parcels/<int:parcel_id>')
 def getParcel(parcel_id):
@@ -182,7 +334,20 @@ def cancel_delivery_order(parcel_id):
     params: parcelid
     returns: cancelled delivery order
     """
+    if len(parcels)==0:
+        return jsonify({
+            'message':'No parcels yet'
+        }),400
+    elif parcel_id>len(parcels):
+        return jsonify({
+            'message':'Parcel does not exist'
+        })
+    
     parcels[parcel_id-1]['status']='canceled'
+    return jsonify({
+        'message':'Parcel status has been changed to canceled',
+        'Parcel':parcels[parcel_id-1]
+    })
 
 
 
@@ -193,7 +358,33 @@ def get_parcels_by_userId(user_id):
     params: user id
     returns: specified user's parcels
     """
-    pass
+    if len(current_admin)==0:
+        return jsonify({
+            'message':'You are not logged in as admin'
+        }),400
+    if len(user_accounts)==0:
+        return jsonify({
+            'message':'No clients in the system yet'
+        }),400
+    if user_id>len(user_accounts):
+        return jsonify({
+            'message':'No client has the id you requested'
+        }),400
+
+    user_parcels=[]
+    for parcel in parcels:
+        if parcel['user_id']==user_id:
+            user_parcels.append(parcel)
+    if len(user_parcels)==0:
+        return jsonify({
+            "message":user_accounts[user_id-1]['username']+" has no parcels yet"
+        })
+
+    return jsonify({
+        "message":user_accounts[user_id-1]['username']+"'s parcels",
+        "parcels":user_parcels
+    })
+
 
 # def validation(parcel_description,client, recipient, pickup_location,Destination):
 #     if not parcel_description or parcel_description.isspace():
