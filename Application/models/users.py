9b502752591a175092445d3410eee2a44bee6e1b
p@rick <frzpete@gmail.com>
2018-11-15 12:53:50 +0300

    Added class to handle users specificallly

---
+++ Application/models/users.py
@@ -0,0 +1,27 @@
+"""This module handles users"""
+
+class Users():
+    users=[]
+    admin=[]
+    def __init__(self, username,email,contact):
+        self.username = username
+        self.email = email
+        self.contact = contact
+    
+    def add_client(self):
+        """
+        This method adds a client to our system
+        params:n/a
+        returns:bool
+        """
+        pass
+    
+    def add_admin(self):
+        """
+        This method adds an admin to our system
+        params:n/a
+        returns: bool
+        """
+        pass
+    
+    
