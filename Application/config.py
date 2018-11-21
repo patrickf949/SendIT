8c3489d2b6afb60f2c902353cafa4c9af87bc956
p@rick <frzpete@gmail.com>
2018-11-21 18:25:52 +0300

    Set different environments for application
    Application has four environments, that is:
    * Testing Environment
    * Development Environment
    * Staging environment
    * Production Environment
    
    Application has 3

--- Application/config.py
+++ Application/config.py
@@ -1,14 +1,20 @@
+"""
+Congigure different environments
+"""
+
+import os
 class Config():
     DEBUG = False
-
+    DB_NAME = 'senditdb'
 
 class DevelopmentConfig(Config):
     DEBUG =  True
-
+    
 
 class TestingConfig(Config):
     TESTING = True
     DEBUG = True
+    DB_NAME = 'test_senditdb'
 
 
 class ProductionConfig(Config):
@@ -19,11 +25,10 @@ class ProductionConfig(Config):
 class StagingConfig(Config):
     DEBUG = True
 
-appConfig = {
+app_config = {
     'development':DevelopmentConfig,
     'testing':TestingConfig,
     'staging':StagingConfig,
     'production':ProductionConfig
 }
 
-dbname= 'senditdb'
