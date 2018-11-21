bf5c9531e8c1869b637290070b3321cfc389e179
p@rick <frzpete@gmail.com>
2018-11-21 18:35:11 +0300

    Fix typo

--- Application/__init__.py
+++ Application/__init__.py
@@ -12,13 +12,13 @@ def create_app(config):
         return jsonify([
             "SendIT!"
             +"SendIT is a courier service that helps users deliver parcels to different destinations. SendIT provides courier quotes based on weight categories.",{
-                "/api/v1/parcels":"Create parcel"
-                ,"/api/v1/parcels": "Fetch all parcels"
-                ,"/api/v1/parcel/<int:parcelId>": "fetch parcel by id"                
-                ,"/api/v1/parcels/<int:parcelId>/cancel": "Cancel a parcel delivery order"
-                ,"/api/v1/users": "View all users"
-                ,"/api/v1/users/<int:userId>/parcels":"Fetch parcels by a specific user"
-                ,"/api/v1/parcels/<int:parcelId>/update":"Update a parcel delivery order"
+                "/api/v2/parcels":"Create parcel"
+                ,"/api/v2/parcels": "Fetch all parcels"
+                ,"/api/v2/parcel/<int:parcelId>": "fetch parcel by id"                
+                ,"/api/v2/parcels/<int:parcelId>/cancel": "Cancel a parcel delivery order"
+                ,"/api/v2/users": "View all users"
+                ,"/api/v2/users/<int:userId>/parcels":"Fetch parcels by a specific user"
+                ,"/api/v2/parcels/<int:parcelId>/update":"Update a parcel delivery order"
             }            
         ])
 
