28482e90e177f650bdc0ecd5c475f5b59a583bad
p@rick <frzpete@gmail.com>
2018-11-21 20:42:54 +0300

    Move database and validator files to controller folder

---
+++ Application/controllers/database.py
@@ -0,0 +1,222 @@
+"""
+Handle database connection
+"""
+
+import psycopg2
+import psycopg2.extras
+
+
+
+class Database():
+    """
+    Handle database connections
+    """
+    dbname=''
+    def __init__(self):
+        """
+        initialise database connection
+        """
+        credentials = """
+        user='senditdb'
+        dbname={}
+        password='sendit123'
+        port=5432
+        host='localhost'
+        """.format(Database.dbname)
+
+        connection = psycopg2.connect(credentials)
+        connection.autocommit = True
+        self.cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        self.create_tables()
+
+
+    def create_tables(self):
+        """
+        Create necessary tables in database
+        params:n/a
+        returns:n/a
+        """
+        sql_command = """
+            CREATE TABLE IF NOT EXISTS users(
+                user_id SERIAL PRIMARY KEY,
+                username VARCHAR (250) NOT NULL UNIQUE,
+                email VARCHAR (250) NOT NULL UNIQUE,
+                contact VARCHAR (250) NOT NULL UNIQUE,
+                password VARCHAR (250) NOT NULL,
+                date_created TIMESTAMP NOT NULL,
+                admin BOOLEAN NOT NULL DEFAULT FALSE
+            );
+            CREATE TABLE IF NOT EXISTS parcels(
+                parcel_id SERIAL PRIMARY KEY,
+                user_id INT NOT NULL,
+                parcel_description VARCHAR (50) NOT NULL,
+                weight_kgs INT NULL,
+                price INT NULL,
+                recipient VARCHAR (250) NOT NULL,
+                recipient_contact VARCHAR (250) NOT NULL,
+                pickup_location VARCHAR (250) NOT NULL,
+                current_location VARCHAR (250) NULL,
+                destination VARCHAR (250) NOT NULL,
+                status varchar(25) check (status in ('pending', 'delivered', 'in transit', 'canceled')) DEFAULT 'pending',
+                date_created TIMESTAMP,
+                date_to_be_delivered TIME
+            );
+            CREATE TABLE IF NOT EXISTS weight_categories(
+                weight_id SERIAL PRIMARY KEY,
+                weight_kgs INT NOT NULL,
+                price INT NULL,
+            );            
+		"""
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def add_parcel(self, parcel):
+        """
+        Add parcel to database
+        params:n/a
+        returns:n/a
+        """
+        sql_command = """
+        INSERT INTO parcels 
+        (user_id, parcel_description, recipient, recipient_contact,
+        pickup_location, current_location, destination, date_created,date_to_be_delivered)
+        values ({user_id},'{recipient}','{contact}','{parcel_description}','{pickup_location}',
+        '{current_location}','{destination}', now(), now()+'2 days 2 hours');
+        """.format(
+            user_id=parcel['user_id'],
+            parcel_description=parcel['parcel_description'],
+            recipient=parcel['recipient'],
+            contact=parcel['contact'],
+            pickup_location=parcel['pickup_location'],
+            current_location=parcel['pickup_location'],
+            destination=['destination']
+        )
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def add_user(self, user):
+        """
+        Add user to database
+        params:n/a
+        returns:n/a
+        """
+        sql_command = """
+        INSERT INTO users (username,email,contact,password,date_created,admin) 
+        values ('{username}','{email}','{contact}','{password}',now(),'{admin}');
+        """.format(
+            username=['username'],
+            email=['email'],
+            contact=['contact'],
+            password=user['password'],
+            admin=user['admin']
+        )
+
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def change_status(self, status, parcel_id):
+        """
+        change status of parcel delivery order
+        params: status and parcel id
+        returns: n/a
+        """
+        sql_command = """
+        UPDATE parcels
+            SET status = '{status}'
+            WHERE 
+            parcel_id={parcel_id};
+        """.format(status=status, parcel_id=parcel_id)
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def change_destination(self, destination, parcel_id):
+        """
+        change status of parcel delivery order
+        params: status and parcel id
+        returns: n/a
+        """
+        sql_command = """
+        UPDATE parcels
+            SET destination = '{destination}'
+            WHERE 
+            parcel_id={parcel_id};
+        """.format(destination=destination, parcel_id=parcel_id)
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def check_availability_of_username(self, username):
+        """
+        Check if user exists during login or signup
+        params: username
+        returns: n/a
+        """
+        sql_command="""
+        SELECT EXISTS(SELECT 1 FROM parcels where username='{}')
+        """.format(username)
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def check_availability_of_email(self, username):
+        """
+        Check if user exists during login or signup
+        params: username
+        returns: n/a
+        """
+        sql_command="""
+        SELECT EXISTS(SELECT 1 FROM parcels where username='{}')
+        """.format(username)
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def validate_password(self,username,password):
+        """
+        Check if password passed is equal to password in database for specific user
+        params:username,password
+        returns:boolean
+        """
+
+
+    def check_availability_of_anyuser(self):
+        """
+        Check if user exists during login or signup
+        params: username
+        returns: n/a
+        """
+        sql_command="""
+        SELECT EXISTS(SELECT * FROM parcels)
+        """
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def check_availability_of_anyparcel(self, username):
+        """
+        Check if user exists during login or signup
+        params: username
+        returns: n/a
+        """
+        sql_command="""
+        SELECT EXISTS(SELECT * FROM parcels)
+        """
+        rows = self.execute_query(sql_command)
+        return rows
+
+
+    def execute_query(self, sql_command):
+        """
+        Execute query
+        params: sql query statement
+        returns: result
+        """
+        self.cursor.execute(sql_command)
+        rows_returned = self.cursor.fetchall()
+        rowcount = self.cursor.rowcount()
+
+        return rows_returned
---
+++ Application/controllers/validate.py
@@ -0,0 +1,263 @@
+"""
+Handles all validation as well as manipulation
+"""
+from Application.models.parcels import Parcels
+from flask import jsonify
+from Application.models.users import Users
+
+
+class Validation():
+    """
+    All validation
+    """
+    
+    def validate_admin_signup(self,data):
+        user_status = self.signup(data)
+        return user_status
+    
+        
+    def validate_user_signup(self,data):
+        user_status = self.signup(data,'client')
+        return user_status
+
+    def signup(self,data,usertype=''):
+        user_id=len(Users.user_accounts)+1
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        
+        temp_user=[username,email,password]
+        
+        valid_data =self.validate_userdata(temp_user)
+
+        if valid_data!=True:
+            return valid_data
+        
+        user =dict(
+            user_id =user_id,
+            username = username,
+            email = email,
+            password = password
+        )
+        if usertype=='client':
+            Users.user_accounts.append(user)
+
+            return jsonify({
+                'message': 'hello! '+user['username']+' Your Account has been created and automatically logged in',
+            }),200
+        else:
+            Users.admin_accounts.append(user)
+
+            return jsonify({
+                'message': 'hello! '+user['username']+' Your Admin Account has been created and automatically logged in',
+            }),200
+
+
+    def validate_userdata(self,temp_list):
+        for element in temp_list:
+            if type(element)!=str:
+                return jsonify({
+                'message':'sorry! All fields must be strings'
+            }),400
+
+            elif not element or element.isspace():
+                return jsonify({
+                'message':'sorry! your username is required and can not be an empty string'
+            }), 400
+        
+        return True
+
+
+    def validate_parcels_by_user(self,user_id):
+        if len(Users.user_accounts)==0:
+            return jsonify({
+                'message':'No clients in the system yet'
+            }),400
+        if user_id>len(Users.user_accounts):
+            return jsonify({
+                'message':'No client has the id you requested'
+            }),400
+
+        user_parcels=[]
+        for parcel in Parcels.parcels:
+            if parcel['user_id']==user_id:
+                user_parcels.append(parcel)
+        if len(user_parcels)==0:
+            return jsonify({
+                "message":Users.user_accounts[user_id-1]['username']+" has no parcels yet"
+            })
+
+        return jsonify({
+            "message":Users.user_accounts[user_id-1]['username']+"'s parcels",
+            "parcels":user_parcels
+        })
+    
+    def validate_cancel_parcel_delivery_order(self,parcel_id):
+        if len(Parcels.parcels)==0:
+            return jsonify({
+                'message':'No parcels yet'
+            }),400
+        elif parcel_id>len(Parcels.parcels):
+            return jsonify({
+                'message':'Parcel does not exist'
+            }),400
+        
+        Parcels.parcels[parcel_id-1]['status']='canceled'
+        return jsonify({
+            'message':'Parcel status has been changed to canceled',
+            'Parcel':Parcels.parcels[parcel_id-1]
+        }),200
+
+
+    def validate_update_parcel_delivery_order(self,data,parcel_id):
+        parcel_exists =self.check_if_parcel_id_exists(parcel_id)
+        if parcel_exists!=True:
+            return parcel_exists
+        parcel_description = data.get('parcel_description')
+        recipient = data.get('recipient')
+        pickup_location = data.get('pickup_location')
+        destination =data.get('destination')        
+
+        if not parcel_description or parcel_description.isspace():
+            return jsonify({
+                'message':'sorry! the parcel_description is required and can not be an empty string'
+            }), 400
+
+        if not recipient or recipient.isspace():
+            return jsonify({
+                'message':'sorry! the recipient is required and can not be an empty string'
+            }), 400
+
+        if not pickup_location or pickup_location.isspace():
+            return jsonify({
+                'message':'sorry! the pickup_location is required and can not be an empty string'
+            }), 400
+
+        
+        parcel_index =parcel_id-1
+        
+        Parcels.parcels[parcel_index]['parcel_description'] = parcel_description
+        Parcels.parcels[parcel_index]['recipient'] = recipient
+        Parcels.parcels[parcel_index]['pickup_location'] = pickup_location
+        Parcels.parcels[parcel_index]['destination'] =destination
+        
+        
+        
+        return jsonify({
+            'Message': 'Parcel has been Updated',
+            'parcel': Parcels.parcels[parcel_index]
+        }),200
+    
+    def validate_parcel_addition(self,data):
+        
+        
+        parcel_id=len(Parcels.parcels)+1
+        parcel_description = data.get('parcel_description')
+        client = data.get('client')
+        user_id=self.get_user_id(client)
+        recipient = data.get('recipient')
+        pickup_location = data.get('pickup_location')
+        destination =data.get('destination')
+        status='pending'
+        temp_parcel = [parcel_description,client,recipient,pickup_location,destination]
+
+        if user_id is False:
+            return jsonify({
+                    'message':'Create an account first'
+                }),400
+        for element in temp_parcel:
+            if type(element)!=str:
+                return jsonify({
+                    'message':'All specifications should be a sequence of characters'
+                }),400
+
+            elif not element or element.isspace():
+                return jsonify({
+                    'message':'All your specifications should be available and should not be a space. Make sure you have the following',
+                    '1. ':'parcel description',
+                    '2. ':'recipient',
+                    '3. ':'recipients contact',
+                    '4. ':'pickup location',
+                    '5. ':'destination'
+                }),400
+
+        parcel =dict(
+            parcel_id=parcel_id,
+            parcel_description = parcel_description,
+            client = client,
+            user_id=user_id,
+            recipient = recipient,
+            pickup_location = pickup_location,
+            destination =destination,
+            status = status
+        )
+        
+
+        Parcels.parcels.append(parcel)
+
+        return jsonify({
+            'message': 'hello! '+Parcels.parcels[-1]['client']+' Your Parcel Delivery order has been placed',
+            'Parcel':Parcels.parcels[-1]
+        }),200
+    
+
+
+    def validate_get_parcel_by_id(self,parcel_id):
+        if self.check_if_parcel_id_exists(parcel_id)!=True:
+            return self.check_if_parcel_id_exists(parcel_id)
+            
+        for parcel in Parcels.parcels:
+            if parcel['parcel_id'] == parcel_id:
+                return jsonify({
+                    'Specified parcel':parcel
+                }), 200
+
+        return jsonify({
+            'message':'the parcel was not found'
+        }), 400
+    
+
+    def validate_get_all_parcels(self):
+        if len(Parcels.parcels)>0:
+            return jsonify({
+            'parcels':Parcels.parcels
+            }),200
+
+        return jsonify({
+            'message':'No parcels added yet'
+        }),400
+
+    
+    def get_user_id(self,client):
+        
+        for user in Users.user_accounts:
+            if user['username']==client:
+                user_id = user['user_id']
+                return user_id
+
+        return False
+    
+
+    def validate_get_all_users(self):
+
+        if len(Users.user_accounts)>0:
+            return jsonify({
+                'Users':Users.user_accounts
+            }),200
+
+        return jsonify({
+            'message':'No users in the system'
+        }),400
+    
+    def check_if_parcel_id_exists(self,parcel_id):
+        if len(Parcels.parcels)==0:
+            return jsonify({
+                'message':'No parcel delivery orders'
+            }),400
+        
+        if parcel_id>len(Parcels.parcels) or parcel_id==0:
+            return jsonify({
+                'message':'invalid parcel id'
+            }),400
+
+        return True
