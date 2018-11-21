b6b3932d3e48363d66917e3aa779efd8fff8b3c2
p@rick <frzpete@gmail.com>
2018-11-21 20:45:55 +0300

    change model to dictionary

--- Application/models/users.py
+++ Application/models/users.py
@@ -1,11 +1,8 @@
 """This module handles users"""
 
 class Users():
-    
-    def __init__(self,username,email,password,contact,admin=False):
-        self.username = username
-        self.email= email
-        self.password = password
-        self.admin = admin
-        self.contact = contact
-    
+    """
+    User model
+    """    
+    def __init__(self,user):
+        self.user = user
