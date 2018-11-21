9e9ebafa27fdbb0b8627d27a4cdc0fcab26d61a5
p@rick <frzpete@gmail.com>
2018-11-21 08:56:55 +0300

    Convert parcel module to parcel storage model

--- Application/models/parcels.py
+++ Application/models/parcels.py
@@ -6,7 +6,7 @@ class Parcels():
     """
     This class handles all parcel delivery order manipulation
     """
-    parcels=[]
+    
     def __init__(self, parcel):
         self.parcel=parcel
     
