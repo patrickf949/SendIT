1d842d32440c5303d664d8dfe264651a28ddb23f
p@rick <frzpete@gmail.com>
2018-11-22 19:08:56 +0300

    Erase storage of jwt token in cookies

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,7 +1,7 @@
-from flask import jsonify,Flask
+from flask import jsonify, Flask, request
 from Application.controllers.database import Database
 from Application.controllers.validate import Validation
-
+import datetime
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,jwt_refresh_token_required,
     create_refresh_token,get_jwt_identity, set_access_cookies,
@@ -9,15 +9,11 @@ from flask_jwt_extended import (
     )
 
 
+app = Flask(__name__)
 
 def create_app(config):
 
     app = Flask(__name__)
-    app.config['JWT_TOKEN_LOCATION'] = ['cookies']
-    app.config['JWT_COOKIE_SECURE'] = False
-    app.config['JWT_COOKIE_PATH'] = '/cookiepath'
-    app.config['JWT_REFRESH_COOKIE_PATH'] = '/me-cookie/fresh'
-    app.config['JWT_COOKIE_CRSF_PROTECT'] = True
     app.config['JWT_SECRET_KEY'] = 'Don-t-you-test-125'
     Validation.dbname = config.dbname
 
@@ -29,6 +25,7 @@ def create_app(config):
             "SendIT!"
             +"SendIT is a courier service that helps users deliver parcels to different destinations. SendIT provides courier quotes based on weight categories.",{
                 "/api/v2/parcels":"Create parcel"
+                ,"/api/v2/signup":"Create parcel"
                 ,"/api/v2/parcels": "Fetch all parcels"
                 ,"/api/v2/parcel/<int:parcelId>": "fetch parcel by id"                
                 ,"/api/v2/parcels/<int:parcelId>/cancel": "Cancel a parcel delivery order"
@@ -56,11 +53,33 @@ def create_app(config):
         logout user and dismiss refresh token
         """
         response = jsonify({'logged out':True})
+    
         unset_jwt_cookies(response)
 
         return response, 200
 
+    
+    @app.route('/api/v2/signup', methods = ['POST'])
+    def signup():
+        data = request.get_json()
+        response = Validation()
+        return response.validate_signup(data)
 
+
+    @app.route('/api/v2/auth/login',methods = ['POST'])
+    def login():
+        response = Validation()
+        data = request.get_json()
+        logged_in = response.validate_user_login(data)
+        if logged_in == True:
+                username = data.get('username')
+                access_token = create_access_token(identity=username, expires_delta=datetime.timedelta(days=1))
+                response = jsonify({'login':True})
+                return jsonify({
+                    'message':'Hello '+username+' you are logged into SendIT',
+                    'Access_token':access_token
+                    }), 200
+        return logged_in
 
     from Application.views import parcels_view
     app.register_blueprint(parcels_view.blue_print)
