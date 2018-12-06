5ef5a95e70ae70e8fd9c1a9e3ce8431716e23f74
p@rick <frzpete@gmail.com>
2018-12-06 18:10:03 +0300

    Remove background image from css

--- Application/ui/assets/js/app.js
+++ Application/ui/assets/js/app.js
@@ -1,4 +1,41 @@
 function signUp(){
+    let username = document.getElementById("username").value;
+    let email = String(document.getElementById("email").value);
+    let contact = String(document.getElementById("contact").value);
+    let password = String(document.getElementById("password").value);
+    
+    console.log(username+", "+password+", "+email+", "+contact)
+
+    let userdetails = {
+        "username":username,
+        "email":email,
+        "contact":contact,
+        "password":password
+    }
+    console.log(userdetails);
+
+    fetch('http://127.0.0.1:5000/api/v2/auth/signup',{
+        method: 'post',
+        headers:{
+            "Content-Type":"application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        body:JSON.stringify(userdetails)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data.message === "hello! "+username+" Your Account has been created. Please login"){
+                alert(data.message);
+                focusLogin();
+            }else{
+                reply = data.message;
+                document.getElementById("api_reply").innerHTML = reply;
+            }
+        }).catch(error => {
+            console.log(error);
+            handleError(error);
+        })
+
     
 }
 
