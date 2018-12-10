351e3d3182eeeca1f4c8441866beeb557a49ab0e
p@rick <frzpete@gmail.com>
2018-12-10 15:06:22 +0300

    Users can login

--- Application/ui/assets/js/app.js
+++ Application/ui/assets/js/app.js
@@ -17,7 +17,8 @@ function signUp(event){
         method: 'POST',
         headers:{
             "Content-Type":"application/json",
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods":"POST"
         },
         body:JSON.stringify(userdetails)
     })
@@ -59,11 +60,19 @@ function loginUser(event){
     })
     .then(response => response.json())
     .then(data => {
-        if(data.message === "hello! "+username+" You are successfully logged into Sendit"){
-
+        reply = data.message;
+        if(data.message === "Hello "+username+" you are logged into SendIT as admin"){
+            localStorage.setItem("usertoken",(data).Access_token);
+            Console.log(localStorage.getItem("usertoken"));
             document.getElementById("api_reply").innerHTML = reply;
+            location.href = "admin_dashboard.html";
+            
+        }else if(data.message === "Hello "+username+" you are logged into SendIT"){
+            localStorage.setItem("usertoken",(data).Access_token);
+            document.getElementById("api_reply").innerHTML = reply;
+            location.href = "dashboard.html"
+            
         }else{
-            reply = data.message;
             document.getElementById("api_reply").innerHTML = reply;
         }
         // document.getElementById("api_reply").innerHTML = reply;
@@ -74,11 +83,70 @@ function loginUser(event){
     
 }
 
-function logout(){
+function logout(event){
+    event.preventDefault()
+    
+    fetch('http://i-sendit.herokuapp.com/api/v2/auth/logout',{
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods":"POST",
+            "Authorization":"Bearer "+localStorage.getItem("usertoken")
+        },
+        body:JSON.stringify(userdetails)
+    })
+    .then(response => response.json())
+    .then(data => {
+        localStorage.setItem("usertoken","");
+    }).catch(error => {
+        console.log(error);
+    })
 
+    
 }
 
-function addParcel(){
+function addParcel(event){
+    event.preventDefault()
+    let parcel = document.getElementById("parcel").value;
+    let recipient = String(document.getElementById("recipient").value);
+    let contact = String(document.getElementById("contact").value);
+    let pickuplocation = String(document.getElementById("pickuplocation").value);
+    let destination = String(document.getElementById("destination").value);
+
+
+    let parceldetails = {
+        "parcel_description":parcel,
+        "recipient":recipient,
+        "contact":contact,
+        "pickup_location":pickuplocation,
+        "destination":destination
+    }
+
+    fetch('http://i-sendit.herokuapp.com/api/v2/parcels',{
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods":"POST",
+            "Authorization":"Bearer "+localStorage.getItem("usertoken")
+        },
+        body:JSON.stringify(userdetails)
+    })
+    .then(response => response.json())
+    .then(data => {
+        reply = data.message;
+        if(data.message.includes("Your Parcel Delivery order has been placed")===true){
+            document.getElementById("api_reply").innerHTML = reply;
+            // location.href = "dashboard.html"
+       
+        }else{
+            document.getElementById("api_reply").innerHTML = reply;
+        }
+        // document.getElementById("api_reply").innerHTML = reply;
+    }).catch(error => {
+        console.log(error);
+    })    
 
 }
 
