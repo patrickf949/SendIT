5a72ff0e9099a6fc665c1b58caab4af9c272c315
p@rick <frzpete@gmail.com>
2018-12-15 13:50:33 +0300

    Add cors to run file

--- run.py
+++ run.py
@@ -3,9 +3,11 @@ Initiate application
 """
 from Application import create_app
 from Application.config import app_config
+from flask_cors import CORS
 
 Config = app_config['production']
 APP = create_app(Config)
+CORS(APP)
 
 if __name__ == '__main__':
     APP.run(debug=Config.DEBUG)
