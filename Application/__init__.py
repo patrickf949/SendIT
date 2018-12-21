de538ff6ff4ed9b738f11b97f78232fc9dce2cbf
p@rick <frzpete@gmail.com>
2018-12-21 20:41:39 +0300

    Pyling

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,3 +1,6 @@
+"""
+Creates application
+"""
 import datetime
 from flask import jsonify, Flask, request, Response, render_template
 from flask_jwt_extended import (
@@ -9,7 +12,10 @@ from Application.controllers.validate import Validation
 
 
 def create_app(config):
-
+    """
+    Creates flask Application
+    params: configuration for current environment
+    """
     app = Flask(__name__)
     app.config.from_object(config)
     app.config['JWT_SECRET_KEY'] = 'Don-t-you-test-125'
@@ -26,24 +32,29 @@ def create_app(config):
     def kingslanding():
         return jsonify([
             "SendIT!"
-            +"SendIT is a courier service that helps users deliver parcels to different destinations."
-            +" SendIT provides courier quotes based on weight categories.",
+            +" SendIT is a courier service that helps users deliver parcels to"+
+            " different destinations."+
+            " SendIT provides courier quotes based on weight categories.",
             {
                 "/api/v2/auth/signup | POST" : "User Sign up",
                 "/api/v2/auth/login | POST" : "User Login",
                 "/api/v2/parcels | POST" : "Create parcel",
                 "/api/v2/parcels | GET" :  "Fetch all parcels",
                 "/api/v2/parcel/<int:parcelId> | GET" : "fetch parcel by id",
-                "/api/v2/parcels/<int:parcelId>/cancel | GET" : "Cancel a parcel delivery order",
+                "/api/v2/parcels/<int:parcelId>/cancel | GET" : "Cancel a par"+
+                "cel delivery order",
                 "/api/v2/users | GET" : "View all users",
-                "/api/v2/users/<int:userId>/parcels | GET" : "Fetch parcels by a specific user - admin",
-                "/api/v2/parcels/<int:parcelId>/destination | PUT" : "Update a parcel delivery order's destination",
-                "/api/v2/parcels/<int:parcelId>/presentLocation | PUT" : "Update the present location of a parcel delivery order",
-                "/api/v2/parcels/<int:parcelId>/status | PUT" : "Update the status of a parel delivery order's destination",
+                "/api/v2/users/<int:userId>/parcels | GET" : "Fetch parcels by"+
+                " a specific user - admin",
+                "/api/v2/parcels/<int:parcelId>/destination | PUT" : "Update "+
+                "a parcel delivery order's destination",
+                "/api/v2/parcels/<int:parcelId>/presentLocation | PUT" :
+                "Update the present location of a parcel delivery order",
+                "/api/v2/parcels/<int:parcelId>/status | PUT" : "Update the sta"
+                +"tus of a parel delivery order's destination",
             }
         ]), 200
 
-    
 
     @app.route("/api/v2/logout", methods=['POST'])
     @jwt_required
@@ -63,7 +74,7 @@ def create_app(config):
         return unique_identifier in blacklist
 
 
-    from Application.views import parcels_view,users_view
+    from Application.views import parcels_view, users_view
 
     app.register_blueprint(parcels_view.blue_print)
     app.register_blueprint(users_view.blue_print)
